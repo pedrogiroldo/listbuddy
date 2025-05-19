da45fc7b0715db33a754edec9e53a2c7a43a9dc4
Pedro Giroldo <pedrogiroldo40@gmail.com>
2025-05-18 22:08:47 -0300

    💄 Add MUI

--- src/renderer/pages/home/page.tsx
+++ src/renderer/pages/home/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@mui/material';
+
 export default function HomePage() {
   return (
-    <div>
+    <div className="w-full flex flex-col justify-center items-center">
       <p className="text-2xl">
         Welcome to the Electron + React + TypeScript + TailwindCSS template!!!!
       </p>
+      <Button variant="contained" onClick={() => alert('world')}>
+        Hello
+      </Button>
     </div>
   );
 }
